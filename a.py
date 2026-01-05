91964ad18fcf59af8ebcadc006cc2f22114db9b8
AlexxBenny <150353944+AlexxBenny@users.noreply.github.com>
2026-01-05 15:15:14 +0530

    Remove API key from genai.Client initialization
    Removed API key from client initialization for security.

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyALzuIA1wjU8pw6yTxnlih22dfAUKXdPcA")
+client = genai.Client(api_key="")
 
 response = client.models.generate_content(
     model="gemini-2.5-flash", contents="unesco full form"
